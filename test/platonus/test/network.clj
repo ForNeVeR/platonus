c0c5780c536dcfdcadf6ea503f2c2ccde144a0b9
ForNeVeR <neverthness@gmail.com>
2012-08-25 19:39:36 +0700

    Added simple phrase test.

--- test/platonus/test/network.clj
+++ test/platonus/test/network.clj
@@ -5,3 +5,11 @@
 (deftest new-network-is-empty
   (let [network (network/create)]
     (is true (empty? network))))
+
+(deftest simple-phrase-test
+  (let [network (-> (network/create)
+                    (network/add-phrase "ave omnissiah"))
+        words   (get network "ave")]
+    (is 3 (count network))
+    (is 1 (count words))
+    (is 1 (get words "omnissiah"))))
