c5da18718f3f4adb877c0ae274a4417dcaf6dc02
ForNeVeR <neverthness@gmail.com>
2012-08-31 11:29:27 +0700

    Started GUI implementation.

---
+++ src/platonus/gui.clj
@@ -0,0 +1,29 @@
+(ns platonus.gui
+  (:use seesaw.core))
+
+(defn- load-file
+  [arg]
+  (alert "Placeholder."))
+
+(defn- create-file-menu
+  []
+  (menu :text  "File"
+        :items [(action :name    "Load..."
+                        :handler load-file)]))
+
+(defn- create-menubar
+  []
+  (menubar :items [(create-file-menu)]))
+
+(defn- create-frame
+  []
+  (frame :title    "Platonus"
+         :content  (create-menubar)
+         :on-close :exit))
+
+(defn -main
+  [& args]
+  (invoke-later
+    (-> (create-frame)
+        pack!
+        show!)))
