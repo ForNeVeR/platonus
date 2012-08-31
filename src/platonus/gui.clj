c237bd7049bb46a9dbed31d7dd06e5620c9cddbc
ForNeVeR <neverthness@gmail.com>
2012-08-31 19:20:36 +0700

    gui: interactions abstracted as agent.

--- src/platonus/gui.clj
+++ src/platonus/gui.clj
@@ -3,58 +3,75 @@
                  core])
   (:require [clojure.string :as string]
             [platonus.filesystem :as filesystem]
-            [platonus.network :as network]))
+            [platonus.network :as network]
+            [platonus.uiagent :as uiagent]))
 
 (defn- load-file-action
-  [network]
+  [model]
   (fn [_]
     (let [file     (choose-file)
           encoding (input "Encoding:")
           nickname (input "Nickname:")]
-      (swap! network filesystem/scan-file nickname file encoding))))
+      (uiagent/transform-network model
+        (fn [network]
+          (filesystem/scan-file network nickname file encoding))))))
 
 (defn- generate-action
-  [network]
+  [model]
   (fn [_]
-    (alert (string/join " " (network/generate @network)))))
+    (uiagent/with-network model
+      (fn [network]
+        (alert (string/join " " (network/generate network)))))))
 
 (defn- create-file-menu
-  [network]
+  [model]
   (menu :text  "File"
         :items [(action :name    "Load..."
-                        :handler (load-file-action network))]))
+                        :handler (load-file-action model))]))
 
 (defn- create-network-menu
-  [network]
-  (menu :text "Network"
+  [model]
+  (menu :text  "Network"
         :items [(action :name "Generate..."
-                        :handler (generate-action network))]))
+                        :handler (generate-action model))]))
 
 (defn- create-menubar
-  [network]
-  (menubar :items [(create-file-menu network)
-                   (create-network-menu network)]))
+  [model]
+  (menubar :items [(create-file-menu model)
+                   (create-network-menu model)]))
 
 (defn- create-statusbar
-  [network]
-  (label "Ready"))
+  [model]
+  (let [label (label "Ready")]
+    (uiagent/add-change-listener model
+     (fn [network]
+       (invoke-now 
+         (config! label :text (str "Records: " (count network))))))
+    label))
 
 (defn- create-main-panel
-  [network]
-  (border-panel :north (create-menubar network)
-                :south (create-statusbar network)))
+  [model]
+  (border-panel :north (create-menubar model)
+                :south (create-statusbar model)))
 
 (defn- create-frame
-  [network]
+  [model]
   (frame :title    "Platonus"
-         :content  (create-main-panel network)
+         :content  (create-main-panel model)
          :on-close :exit))
+
+(defn publish-error
+  [^Throwable error]
+  (.printStackTrace error))
 
 (defn -main
   [& args]
-  (let [network (atom (network/create))]
+  (let [model (uiagent/create)]
+    (uiagent/set-error-listener model
+      (fn [_ error]
+        (publish-error error)))
     (invoke-later
-      (-> network
+      (-> model
           create-frame
           pack!
           show!))))
