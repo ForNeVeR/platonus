bfe52235fb0570f96a55672099dd022e50e050fa
ForNeVeR <neverthness@gmail.com>
2012-08-31 13:09:47 +0700

    gui: enabled file loading and network generation.

--- src/platonus/gui.clj
+++ src/platonus/gui.clj
@@ -1,38 +1,60 @@
 (ns platonus.gui
-  (:use seesaw.core))
+  (:use [seesaw chooser
+                 core])
+  (:require [clojure.string :as string]
+            [platonus.filesystem :as filesystem]
+            [platonus.network :as network]))
 
 (defn- load-file-action
-  [arg]
-  (alert "Placeholder."))
+  [network]
+  (fn [_]
+    (let [file     (choose-file)
+          encoding (input "Encoding:")
+          nickname (input "Nickname:")]
+      (swap! network filesystem/scan-file nickname file encoding))))
+
+(defn- generate-action
+  [network]
+  (fn [_]
+    (alert (string/join " " (network/generate @network)))))
 
 (defn- create-file-menu
-  []
+  [network]
   (menu :text  "File"
         :items [(action :name    "Load..."
-                        :handler load-file-action)]))
+                        :handler (load-file-action network))]))
+
+(defn- create-network-menu
+  [network]
+  (menu :text "Network"
+        :items [(action :name "Generate..."
+                        :handler (generate-action network))]))
 
 (defn- create-menubar
-  []
-  (menubar :items [(create-file-menu)]))
+  [network]
+  (menubar :items [(create-file-menu network)
+                   (create-network-menu network)]))
 
 (defn- create-statusbar
-  []
+  [network]
   (label "Ready"))
 
 (defn- create-main-panel
-  []
-  (border-panel :north (create-menubar)
-                :south (create-statusbar)))
+  [network]
+  (border-panel :north (create-menubar network)
+                :south (create-statusbar network)))
 
 (defn- create-frame
-  []
+  [network]
   (frame :title    "Platonus"
-         :content  (create-main-panel)
+         :content  (create-main-panel network)
          :on-close :exit))
 
 (defn -main
   [& args]
-  (invoke-later
-    (-> (create-frame)
-        pack!
-        show!)))
+  (let [network (atom (network/create))]
+    (invoke-later
+      (-> network
+          create-frame
+          pack!
+          show!))))
