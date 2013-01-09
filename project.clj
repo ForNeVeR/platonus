da7e41d8e3424611f418c96dc9e2f8657fd114d2
ForNeVeR <neverthness@gmail.com>
2013-01-09 20:43:42 +0700

    Project: changed version and description. We're ready to release.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject platonus "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject platonus "0.1"
+  :description "Simple Markov network library."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.googlecode.soundlibs/mp3spi "1.9.5-1"]
                  [seesaw "1.4.2"]]
