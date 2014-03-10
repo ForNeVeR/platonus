c7f4dd4a93344e44cdb561c0cc08f002dca61112
ForNeVeR <neverthness@gmail.com>
2014-03-10 15:33:26 +0700

    Network: refactor tests, add test for depth 2.

--- src/test/scala/me/fornever/platonus/tests/NetworkSpec.scala
+++ src/test/scala/me/fornever/platonus/tests/NetworkSpec.scala
@@ -13,19 +13,28 @@ class NetworkSpec extends FlatSpec with Matchers {
   }
 
   it should "have size of 2 after adding the one-word phrase" in {
-    var network = Network()
     val phrase = Vector("word")
-    network = network.add(phrase)
+    val network = Network().add(phrase)
     network.size should equal (2)
   }
 
   it should "have a specialized one-layer structure when added a phrase" in {
-    var network = Network()
     val phrase = Vector("word")
-    network = network.add(phrase)
+    val network = Network().add(phrase)
     val structure = Map(
       Vector(PhraseBegin()) -> Map(OrdinarWord("word") -> 1),
       Vector(OrdinarWord("word")) -> Map(PhraseEnd() -> 1)
+    )
+
+    network.data should equal (structure)
+  }
+
+  it should "have a specialized structure of depth 2 when added a phrase" in {
+    val phrase = Vector("1", "2")
+    val network = Network(2).add(phrase)
+    val structure = Map(
+      Vector(PhraseBegin(), OrdinarWord("1")) -> Map(OrdinarWord("2") -> 1),
+      Vector(OrdinarWord("1"), OrdinarWord("2")) -> Map(PhraseEnd() -> 1)
     )
 
     network.data should equal (structure)
