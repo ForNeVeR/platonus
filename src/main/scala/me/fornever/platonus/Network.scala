8581b8b0b632e7a9efa1e8b21ac2e1e58ce795d8
ForNeVeR <neverthness@gmail.com>
2014-03-10 18:28:14 +0700

    Netword: add generate method.

--- src/main/scala/me/fornever/platonus/Network.scala
+++ src/main/scala/me/fornever/platonus/Network.scala
@@ -7,6 +7,8 @@ object Network {
 }
 
 class Network(val depth: Int, val data: Map[Vector[Word], Map[Word, Int]]) {
+  def size = data.size
+
   def add(phrase: Vector[String]): Network = {
     val words = Stream.concat(
       Stream(PhraseBegin()),
@@ -23,7 +25,28 @@ class Network(val depth: Int, val data: Map[Vector[Word], Map[Word, Int]]) {
     Network(depth, data ++ phraseData)
   }
 
-  def size = data.size
+  def generate(): Stream[String] = {
+    def generate(init: Vector[Word]): Vector[Word] = {
+      val key = init.takeRight(depth)
+      val nextWordMap = data.getOrElse(key, Map())
+      val nextWord = if (nextWordMap.isEmpty) {
+        None
+      } else {
+        Some(nextWordMap.maxBy(_._2)._1)
+      }
+
+      nextWord match {
+        case Some(word @ OrdinarWord(_)) => generate(init :+ word)
+        case _ => init :+ PhraseEnd()
+      }
+    }
+
+    val phrase = generate(Vector(PhraseBegin()))
+    phrase.toStream.filter(_.isInstanceOf[OrdinarWord]).map {
+      case OrdinarWord(word) => word
+      case _ => throw new Exception("Impossible")
+    }
+  }
 
   private def getWordsKey(words: Stream[Word]) = words.take(depth).toVector
   private def prepareWordValue(item: (Vector[Word], List[Stream[Word]])): (Vector[Word], Map[Word, Int]) = {
